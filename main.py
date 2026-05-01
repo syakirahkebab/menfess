bf35498731576d76860e61a38cf8f2d63824cdad
syakirahkebab <syakirahkebab.official@gmail.com>
2026-05-01 22:24:57 +0700

    Fix AsyncIOScheduler startup without running event loop

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from bot import Bot, data, Database
+from bot import Bot
 
-async def reset_menfess():
-    db = Database(data[0])
-    x = await db.reset_menfess()
-    await Bot().kirim_pesan(x=str(x))
-    print('BOT BERHASIL DIRESET')
 
-scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
-scheduler.add_job(reset_menfess, trigger="cron", hour=1, minute=0)
-scheduler.start()
-
-Bot().run()
-
+if __name__ == '__main__':
+    Bot().run()
